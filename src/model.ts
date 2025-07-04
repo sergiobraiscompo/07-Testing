ea2586595d0557e04d1d37cb029a47388aedb448
Sergio <108098770+sergiobraiscompo@users.noreply.github.com>
2025-07-04 19:33:02 +0200

    Update model.ts

--- src/model.ts
+++ src/model.ts
@@ -1,3 +1,4 @@
+// Objeto Partida
 interface Partida {
     puntuacion: number;
     carta: number;
@@ -5,9 +6,25 @@ interface Partida {
     partidaAcabada: boolean
 }
 
+// Nueva partida
 export const partida: Partida = {
     puntuacion: 0,
     carta: 0,
     mensaje: "",
     partidaAcabada: false
 }
+
+// Baraja de cartas
+export const cartas = [
+    "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/back.jpg",
+    "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/1_as-copas.jpg",
+    "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/2_dos-copas.jpg",
+    "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/3_tres-copas.jpg",
+    "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/4_cuatro-copas.jpg",
+    "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/5_cinco-copas.jpg",
+    "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/6_seis-copas.jpg",
+    "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/7_siete-copas.jpg",
+    "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/10_sota-copas.jpg",
+    "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/11_caballo-copas.jpg",
+    "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/12_rey-copas.jpg"
+]
